47855c932664757d6e4b52904c45a0c48e4df20e
Kai Li Wang <lwang16@unm.edu>
2024-09-26 07:01:30 -0600

    sig implementation

--- PA02/PA02sig.c
+++ PA02/PA02sig.c
@@ -1,2 +1,69 @@
-int main(int argc, char ** argv) {
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+pid_t child_pid;
+bool child_running = true;
+
+// signal handler for SIGINT (Ctrl+C)
+void handle_sigint() {
+  if (child_pid > 0) {
+    printf("\nSIGINT caught, stopping child process %d...\n", child_pid);
+    kill(child_pid, SIGKILL);
+  }
+  exit(EXIT_SUCCESS);
+}
+
+// signal handler for SIGTSTP (Ctrl+Z)
+void handle_sigtstp() {
+  const char *action = child_running ? "stopping" : "running";
+  printf("\nSIGTSTP caught, %s child process %d...\n", action, child_pid);
+  kill(child_pid, child_running ? SIGSTOP : SIGCONT);
+
+  action = child_running ? "suspended" : "running";
+  printf("child process now %s\n", action);
+
+  child_running = !child_running;
+}
+
+int main(void) {
+  // set up signal handlers for SIGINT and SIGTSTP
+  struct sigaction sa_int, sa_tstp;
+
+  sa_int.sa_handler = handle_sigint;
+  sa_int.sa_flags = 0;
+  sigemptyset(&sa_int.sa_mask);
+  if (sigaction(SIGINT, &sa_int, NULL) == -1) {
+    perror("error setting up SIGINT handler");
+    exit(EXIT_FAILURE);
+  }
+
+  sa_tstp.sa_handler = handle_sigtstp;
+  sa_tstp.sa_flags = 0;
+  sigemptyset(&sa_tstp.sa_mask);
+  if (sigaction(SIGTSTP, &sa_tstp, NULL) == -1) {
+    perror("error setting up SIGTSTP handler");
+    exit(EXIT_FAILURE);
+  }
+
+  // fork and execve to start the "yes" process in child
+  child_pid = fork();
+  if (child_pid == -1) {
+    perror("error forking");
+    exit(EXIT_FAILURE);
+  } else if (child_pid == 0) {
+    // child execute "yes"
+    char *args[] = {"/bin/yes", NULL};
+    execve("/bin/yes", args, NULL);
+    perror("execve failed");  // if execve fails
+    exit(EXIT_FAILURE);
+  }
+
+  // parent enter an infinite loop and wait for signals
+  while (1) {
+    pause();  // Wait for signals
+  }
 }
